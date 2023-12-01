63bcff7bb4a34f1a230e330c4777d85b8e59da73
Jose Luis Villca Villegas <82187040+jvillcavillegas@users.noreply.github.com>
2023-12-01 18:57:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bienvenido" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Ingresar a la web academica de:<a href="https://jvillcavillegas.github.io/">Jose Luis Villca Villegas</a>
+
         </p>
       </main>
 
